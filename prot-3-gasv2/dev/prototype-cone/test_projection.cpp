befcd51bc363a9f8a5693a99bf89e25dafdde379
Marc Closa <marcclosa8@gmail.com>
2018-11-08 13:15:41 +0100

    Test projection file advanced version

--- prot-3-gasv2/dev/prototype-cone/test_projection.cpp
+++ prot-3-gasv2/dev/prototype-cone/test_projection.cpp
@@ -22,29 +22,104 @@ std::vector<sf::Vector2f> getFootprint(void) const
     float c_len = R * cos(alpha);
     /* Computation of cross product */
     sf::Vector3f p_n = p / h;
-    std::vector<float> v = {p_n.x, p_n.y, p_n.z};
+    sf::Vector3f v = sf::Vector3f(p_n.x, p_n.y, p_n.z);
     sf::Vector3f c = v * c_len;
-    std::vector<float> a = {0, v[3], -v[2]};
-    std::vector<float> b (3);
 
-    b[0] = v[1] * a[2] - v[2] * a[1];
-    b[1] = v[0] * a[2] - v[2] * a[0];
-    b[2] = v[0] * a[1] - v[1] * a[0];
+    /* When v = [1 0 0], a will be a = [0 0 0] using our perpendicular vector generator method.
+     * For this reason, we check that v = [1 0 0] and in this case we force a = [0 1 0] that is
+     * always perpendicular to v. And then, we generate b with the cross product.
+     **/
+
+    if(v == sf::Vector3f(1, 0, 0)) {
+        sf::Vector3f a = sf::Vector3f(0, 1, 0);
+    } else {
+        sf::Vector3f a = sf::Vector3f(0, v.z, -v.y);
+    }
+
+    a = a / std::sqrt(a.x * a.x + a.y * a.y + a.z * a.z);
+    sf::Vector3f b = sf::Vector3f();
+
+    b.x = v.y * a.z - v.z * a.y;
+    b.y = v.x * a.z - v.z * a.x;
+    b.z = v.x * a.y - v.y * a.x;
+
+    b = b / std::sqrt(b.x * b.x + b.y * b.y + b.z * b.z);
+
 
     float step = 2 * Config::pi / N_POINTS;
 
     std::vector<sf::Vector3f> positions;
 
-    for(float th = 0; th < N_POINTS; th += step) {
-        float footprint_x = c.x + footprint_radius * std::cos(th) * a[1] + footprint_radius * std::sin(th) * b[1];
-        float footprint_y = c.z + footprint_radius * std::cos(th) * a[2] + footprint_radius * std::sin(th) * b[2];
-        float footprint_z = c.z + footprint_radius * std::cos(th) * a[3] + footprint_radius * std::sin(th) * b[3];
+    sf::Vector3f n_pole = sf::Vector3f(0, 0, R);
+    sf::Vector3f s_pole = sf::Vector3f(0, 0, -R);
 
-        sf::Vector3f fp_pos = sf::Vector3f(footprint_x, footprint_y, footprint_z);
-        positions.push_back(fp_pos);
+
+    bool north_included = false;
+    bool south_included = false;
+
+    float distance_to_north = std::sqrt((c.x - n_pole.x) * (c.x - n_pole.x) + (c.y - n_pole.y) * (c.y - n_pole.y)
+        + (c.z - n_pole.z) * (c.z - n_pole.z));
+    float distance_to_south = std::sqrt((c.x - s_pole.x) * (c.x - s_pole.x) + (c.y - s_pole.y) * (c.y - s_pole.y)
+            + (c.z - s_pole.z) * (c.z - s_pole.z));
+
+    if(distance_to_north < footprint_radius) {
+        north_included = true;
+    } else if(distance_to_south < footprint_radius) {
+        south_included = true;
     }
 
-    /*Here, all XYZ points of the circumference will be stored in a vector */
+    for(float th = 0; th < N_POINTS; th += step) {
+        float footprint_x = c.x + footprint_radius * std::cos(th) * a.x + footprint_radius * std::sin(th) * b.x;
+        float footprint_y = c.z + footprint_radius * std::cos(th) * a.y + footprint_radius * std::sin(th) * b.y;
+        float footprint_z = c.z + footprint_radius * std::cos(th) * a.z + footprint_radius * std::sin(th) * b.z;
+
+        sf::Vector3f fp_pos = sf::Vector3f(footprint_x, footprint_y, footprint_z);
+        sf::Vector2f proj   = AgentMotion::getProjection2D(fp_pos, VirtualTime::now());
+
+        sf::Vector2f prev = positions.back();
+
+        if(north_included) {
+            if(prev_pos < proj){
+                float border_y = (prev.y - proj.y) / 2;
+            } else {
+                float border_y = (proj.y - prev.y) / 2;
+            }
+
+            if(prev.x > proj.x) {
+                positions.push_back(sf::Vector2f(Config::world_width, border_y));
+                positions.push_back(sf::Vector2f(Config::world_width, Config::world_height));
+                positions.push_back(sf::Vector2f(0, Config::world_height));
+                positions.push_back(sf::Vector2f(0, border_y));
+            } else {
+                positions.push_back(sf::Vector2f(0, border_y));
+                positions.push_back(sf::Vector2f(0, Config::world_height));
+                positions.push_back(sf::Vector2f(Config::world_width, Config::world_height));
+                positions.push_back(sf::Vector2f(Config::world_width, border_y));
+            }
+        }
+
+        if(south_included) {
+            if(prev_pos < proj){
+                float border_y = (prev.y - proj.y) / 2;
+            } else {
+                float border_y = (proj.y - prev.y) / 2;
+            }
+
+            if(prev.x > proj.x) {
+                positions.push_back(sf::Vector2f(Config::world_width, border_y));
+                positions.push_back(sf::Vector2f(Config::world_width, 0));
+                positions.push_back(sf::Vector2f(0, 0));
+                positions.push_back(sf::Vector2f(0, border_y));
+            } else {
+                positions.push_back(sf::Vector2f(0, border_y));
+                positions.push_back(sf::Vector2f(0, 0));
+                positions.push_back(sf::Vector2f(Config::world_width, 0));
+                positions.push_back(sf::Vector2f(Config::world_width, border_y));
+            }
+        }
+
+        if()
+    }
 
     return positions;
 }
