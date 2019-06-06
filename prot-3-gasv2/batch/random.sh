c661bf746c69b9640cc1deff3350cea3f50d1b0a
Carles Araguz <carles.araguz@gmail.com>
2019-06-06 16:35:57 +0200

    Changes on the server side for batch config files

--- prot-3-gasv2/batch/random.sh
+++ prot-3-gasv2/batch/random.sh
@@ -1,7 +1,6 @@
-OMP_NUM_THREADS=10 ../bin/prot-3 --simple-log -g0 -f ../batch/set/dbg_goal/dbg_goal_1bl.yml -l ../data/2019_05_19_072002_dbg_goal_1bl_0/system.yml  --random -d ../data/2019_05_19_072002_dbg_goal_1bl_rand/ > job_logs/dbg_goal_1bl_rand.log 2>&1 &
-OMP_NUM_THREADS=10 ../bin/prot-3 --simple-log -g0 -f ../batch/set/dbg_goal/dbg_goal_2as.yml -l ../data/2019_05_18_205551_dbg_goal_2as_1/system.yml  --random -d ../data/2019_05_18_205551_dbg_goal_2as_rand/ > job_logs/dbg_goal_2as_rand.log 2>&1 &
-OMP_NUM_THREADS=10 ../bin/prot-3 --simple-log -g0 -f ../batch/set/dbg_goal/dbg_goal_2bs.yml -l ../data/2019_05_18_232845_dbg_goal_2bs_0/system.yml  --random -d ../data/2019_05_18_232845_dbg_goal_2bs_rand/ > job_logs/dbg_goal_2bs_rand.log 2>&1 &
-OMP_NUM_THREADS=10 ../bin/prot-3 --simple-log -g0 -f ../batch/set/dbg_goal/dbg_goal_2cs.yml -l ../data/2019_05_19_015716_dbg_goal_2cs_0/system.yml  --random -d ../data/2019_05_19_015716_dbg_goal_2cs_rand/ > job_logs/dbg_goal_2cs_rand.log 2>&1 &
-echo -n "Waiting for the jobs to complete..."
-wait $(jobs -p)
+echo -n "Running 5b_rand... "
+../bin/prot-3 --simple-log -g0 -f ../batch/set/energy/energy_5b.yml -l ../data/2019_05_28_044002_energy_5b_0/system.yml  --random -d ../data/2019_05_28_044002_energy_5b_rand/ > job_logs/energy_5b_rand.log 2>&1
+echo "done."
+echo -n "Running 5c_rand... "
+../bin/prot-3 --simple-log -g0 -f ../batch/set/energy/energy_5c.yml -l ../data/2019_05_28_150856_energy_5c_1/system.yml  --random -d ../data/2019_05_28_150856_energy_5c_rand/ > job_logs/energy_5c_rand.log 2>&1
 echo " done."
